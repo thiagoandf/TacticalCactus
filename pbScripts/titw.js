53e4eac858d7475f3c9e3bcdc98bb272360bd218
Feliciano, Thiago <felicianot@ryanair.com>
2021-02-19 16:09:56 +0000

    This is not the way

--- pbScripts/titw.js
+++ pbScripts/titw.js
@@ -2,7 +2,7 @@ module.exports = (message) => {
   if (message.text.toLowerCase() === 'this is the way')
     return {
       type: 'sendMessage',
-      message: '```\nThis is the way\n```',
+      message: Math.random() < 0.2 ? '```This is not the way, go back!```' : '```\nThis is the way\n```',
       options: {
         parse_mode: 'MarkdownV2'
       }
